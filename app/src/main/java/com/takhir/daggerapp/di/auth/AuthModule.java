00d2cc8505eceee7efe0b0e590555b5aa75f8399
takhir-nafikov <takhir.learn@gmail.com>
2021-01-26 21:53:50 +0300

    Retrofit and auth

---
+++ app/src/main/java/com/takhir/daggerapp/di/auth/AuthModule.java
@@ -0,0 +1,16 @@
+package com.takhir.daggerapp.di.auth;
+
+import com.takhir.daggerapp.network.auth.AuthApi;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+
+@Module
+public class AuthModule {
+
+    @Provides
+    static AuthApi provideAuthApi(Retrofit retrofit) {
+        return retrofit.create(AuthApi.class);
+    }
+}
--- app/src/main/java/com/takhir/daggerapp/ui/auth/AuthActivity.java
+++ app/src/main/java/com/takhir/daggerapp/ui/auth/AuthActivity.java
@@ -2,29 +2,38 @@ package com.takhir.daggerapp.ui.auth;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.bumptech.glide.RequestManager;
 import com.takhir.daggerapp.R;
+import com.takhir.daggerapp.models.User;
 import com.takhir.daggerapp.viewmodels.ViewModelsProviderFactory;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class AuthActivity extends DaggerAppCompatActivity {
+public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AuthActivity";
 
     private AuthViewModel viewModel;
+
+    private EditText userId;
 
     @Inject
     ViewModelsProviderFactory providerFactory;
 
     @Inject
     Drawable logo;
+
     @Inject
     RequestManager requestManager;
 
@@ -32,15 +41,48 @@ public class AuthActivity extends DaggerAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        userId = findViewById(R.id.user_id_input);
+
+        findViewById(R.id.login_button).setOnClickListener(this);
 
         viewModel = ViewModelProviders.of(this, providerFactory).get(AuthViewModel.class);
 
         setLogo();
+
+        subscribeObserve();
+    }
+
+    private void subscribeObserve() {
+        viewModel.observeUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                if (user != null) {
+                    Log.d(TAG, "onChanged: " + user.getEmail());
+                }
+            }
+        });
     }
 
     private void setLogo() {
         requestManager
                 .load(logo)
                 .into((ImageView) findViewById(R.id.login_logo));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button: {
+                attemptLogin();
+                break;
+            }
+        }
+    }
+
+    private void attemptLogin() {
+        if (TextUtils.isEmpty(userId.getText().toString())) {
+            return;
+        }
+        viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
     }
 }
