b2f02281d3aa377050736330fad754a797c1a828
takhir-nafikov <takhir.learn@gmail.com>
2021-02-09 14:57:50 +0300

    Auth and Main scopes

--- app/src/main/java/com/takhir/daggerapp/di/ActivityBuildersModule.java
+++ app/src/main/java/com/takhir/daggerapp/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.takhir.daggerapp.di;
 
 import com.takhir.daggerapp.di.auth.AuthModule;
+import com.takhir.daggerapp.di.auth.AuthScope;
 import com.takhir.daggerapp.di.auth.AuthViewModelModule;
 import com.takhir.daggerapp.di.main.MainFragmentBuildersModule;
 import com.takhir.daggerapp.di.main.MainModule;
+import com.takhir.daggerapp.di.main.MainScope;
 import com.takhir.daggerapp.di.main.MainViewModelsModule;
 import com.takhir.daggerapp.ui.auth.AuthActivity;
 import com.takhir.daggerapp.ui.main.MainActivity;
@@ -15,11 +17,13 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector(
             modules = {AuthViewModelModule.class, AuthModule.class}
     )
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {MainFragmentBuildersModule.class, MainViewModelsModule.class, MainModule.class}
     )
